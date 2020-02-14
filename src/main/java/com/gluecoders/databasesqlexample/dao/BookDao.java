c70517082c065774324c93f64125114701404e97
Anand Rajneesh <anandrajneesh@outlook.com>
2020-02-14 14:39:57 +0530

    [Refactor] Added @Repository annotations, though it was working fine without these too

--- src/main/java/com/gluecoders/databasesqlexample/dao/BookDao.java
+++ src/main/java/com/gluecoders/databasesqlexample/dao/BookDao.java
@@ -3,6 +3,8 @@ package com.gluecoders.databasesqlexample.dao;
 import com.gluecoders.databasesqlexample.models.Book;
 import com.gluecoders.databasesqlexample.models.BookId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookDao extends JpaRepository<Book, BookId> {
 }
